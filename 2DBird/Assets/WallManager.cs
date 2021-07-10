ae9bb49b9856d80c13d17594433be6f6e04162ab
Tsuguhiko Okada <tsugu.pc.academy@gmail.com>
2021-07-10 15:01:42 +0900

    7/10の課題完了。（後は、自身で適宜変更した)

---
+++ 2DBird/Assets/WallManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallManager : MonoBehaviour
+{
+    [SerializeField] private float interval = 2;
+                     
+    float nextSpawnTime;
+
+    [SerializeField] GameObject[] walls;
+
+    [SerializeField] GameObject spawner;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (nextSpawnTime<Time.timeSinceLevelLoad)
+        {
+            nextSpawnTime = Time.timeSinceLevelLoad + interval;
+            int id = Random.Range(0, walls.Length);
+            GameObject obj = (GameObject)Instantiate(walls[id],
+                spawner.transform.position,
+                spawner.transform.rotation);
+        }
+    }
+}
